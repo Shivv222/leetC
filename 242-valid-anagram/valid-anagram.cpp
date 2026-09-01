16619245854f3e4a16f597900cb53544e3bbbbd0
Shivv222 <btech10464.23@bitmesra.ac.in>
2026-09-02 02:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (49.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s), end(s));
-        sort(begin(t), end(t));
+        vector<int>count (26, 0);
 
-        return s==t;
+        for(char &ch:s){
+            count[ch-'a']++;
+        } 
+
+        for(char &ch:t){
+            count[ch-'a']--;
+        }
+        bool allzeroes = all_of(begin(count), end(count), [](int element){
+            return element==0;
+        });
+        return allzeroes;
     }
 };
